9f6363154696ccd42b0cc52d6980d53cf70516b0
Guido Arce <guidoarce89@gmail.com>
2018-03-25 21:17:28 -0400

    Add ReactJS library

---
+++ src/entries/home.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Home from "../pages/containers/home";
+
+const home = document.getElementById('home');
+
+ReactDOM.render(<Home />, home);
